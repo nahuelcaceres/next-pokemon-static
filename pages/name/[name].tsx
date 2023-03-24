b256beb79f791ab6720b4b3bf23bea935d74ce52
Nahuel <ncaceres@andreani.com>
2023-03-24 16:00:25 -0300

    ISG by poke name

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -125,7 +125,8 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemonNames.map((name) => ({
       params: { name },
     })),
-    fallback: false,
+    // fallback: false,
+    fallback: 'blocking',
   }
 }
 
@@ -133,10 +134,22 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { name } = params as { name: string }
 
+  const pokemon = await getPokemonInfo(name)
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false, //puede que en el futuro exista un pokemon mas adelante con ese id
+      },
+    }
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo(name),
+      pokemon: pokemon,
     },
+    revalidate: 86400, //(24 horas en segundos)
   }
 }
 
